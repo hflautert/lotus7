a1fe246659b492fa2e48883fa94e5646792944a7
Henrique Fell Lautert <hflautert@gmail.com>
2015-04-07 15:58:56 -0300

    Create install_squid.sh
    Arquivo para automatizar a instalação do squid.

---
+++ squid/install_squid.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Instalação automatizada do Squid (Basico)
+# hflautert@gmail.com
+# 07 de Abril de 2015
+
+# Variaveis
+IP=$(ip a | grep "inet " | grep -v "127.0.0.1" | awk {'print $2'} | cut -d/ -f1)
+
+# Inicialmente desativa o firewall padrao
+# TODO - Criar regras especificas.
+systemctl disable firewalld
+systemctl stop firewalld
+
+# Instalar squid
+yum install squid -y
+
+# Ativar servico na inicializacao
+systemctl enable squid.service
+
+# Inicia servico
+systemctl start squid.service
+
+echo "
+
+O squid foi instalado.
+
+Para conferir o serviço digite:
+systemctl status squid.service
+
+Configure o proxy da estação com:
+$IP:3128
+
+Para conferir os logs:
+tailf /var/log/squid/access.log
+
+"
