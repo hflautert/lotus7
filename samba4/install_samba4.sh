d4ead9b40e623f13a94b459f1e84a00a2bf6e6c7
Henrique Fell Lautert <hflautert@gmail.com>
2015-08-11 10:23:24 -0300

    Update install_samba4.sh

--- samba4/install_samba4.sh
+++ samba4/install_samba4.sh
@@ -1,5 +1,5 @@
 # Adicionar repo
-wget -O /etc/yum.repos.d/sernet-samba.repo https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/rhel/7/sernet-samba-4.2.repo
+wget -O /etc/yum.repos.d/sernet-samba.repo https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/rhel/7/sernet-samba-4.2.repo --no-check-certificate
 
 # Alterar credenciais repo
 sed -i 's/USERNAME:ACCESSKEY/sernet-samba-public:Noo1oxe4zo/g' /etc/yum.repos.d/sernet-samba.repo
