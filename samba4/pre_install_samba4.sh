1b38b9f8cb81e42ebe0a86062a4f9cc54e99aa4a
Henrique Fell Lautert <hflautert@gmail.com>
2015-08-12 14:50:02 -0300

    Update pre_install_samba4.sh

--- samba4/pre_install_samba4.sh
+++ samba4/pre_install_samba4.sh
@@ -1,13 +1,8 @@
 # Adicionar repo
-wget -O /etc/yum.repos.d/sernet-samba.repo https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/rhel/7/sernet-samba-4.2.repo --no-check-certificate
+wget -O /etc/yum.repos.d/sernet-samba.repo https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/rhel/7/sernet-samba-4.2.repo
 
 # Alterar credenciais repo
 sed -i 's/USERNAME:ACCESSKEY/sernet-samba-public:Noo1oxe4zo/g' /etc/yum.repos.d/sernet-samba.repo
-
-# Erro certificado - contorno:
-# vim /etc/yum.repos.d/sernet-samba.repo
-# sslverify=0
-echo "sslverify=0" >> /etc/yum.repos.d/sernet-samba.repo
 
 # Instalação de pacotes
 yum -y install sernet-samba sernet-samba-ad
