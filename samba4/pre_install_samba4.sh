6792283e5ef867e87b507bfb00612de6d12a56e5
Henrique Fell Lautert <hflautert@gmail.com>
2015-08-12 14:23:43 -0300

    Update pre_install_samba4.sh

--- samba4/pre_install_samba4.sh
+++ samba4/pre_install_samba4.sh
@@ -4,15 +4,23 @@ wget -O /etc/yum.repos.d/sernet-samba.repo https://sernet-samba-public:Noo1oxe4z
 # Alterar credenciais repo
 sed -i 's/USERNAME:ACCESSKEY/sernet-samba-public:Noo1oxe4zo/g' /etc/yum.repos.d/sernet-samba.repo
 
-# Instalação de pacotes
 # Erro certificado - contorno:
 # vim /etc/yum.repos.d/sernet-samba.repo
 # sslverify=0
+echo "sslverify=0" >> /etc/yum.repos.d/sernet-samba.repo
+
+# Instalação de pacotes
 yum -y install sernet-samba sernet-samba-ad
 
 # Promover o domínio
 echo "Samba instalado:"
 samba -V
+echo "Antes de provomer o domínio, ajuste seu DNS.
+DNS1=x.x.x.x - Ip do proprio servidor.
+DNS2=x.x.x.x - Ip de outro servidor DNS.
+Search domain = dominio.com.br
+A configuração pode ser feita pelo utilitário nmtui"
+
 echo "Para promover o domínio use:"
 echo "samba-tool domain provision"
-echo "https://wiki.samba.org/index.php/Samba_AD_DC_HOWTO"
+echo "Para maiores informações: https://wiki.samba.org/index.php/Samba_AD_DC_HOWTO"
